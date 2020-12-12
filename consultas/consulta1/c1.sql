4d355d1146c1f8cbf07645f0c61fa6a139be95bc
mayra-diaz <mayra.diaz@utec.edu.pe>
2020-12-12 18:04:36 -0500

    Consultas

---
+++ consultas/consulta1/c1.sql
@@ -0,0 +1,5 @@
+-- Cuántas horas trabajaron por máquina los 10 empleados de mayor sueldo del área de fabricación.
+
+SET SCHEMA 'RT1000';
+
+
---
+++ consultas/consulta2/c2.sql
@@ -0,0 +1,3 @@
+-- Todos los bienes que sean de la categoría 'Construcción', que cuenten con stock (mayor a 0) y hayan sido compradas en el 2019 por un precio unitario mayor a 10 soles.
+   
+   

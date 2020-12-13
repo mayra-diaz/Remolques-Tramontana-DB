a552dd4a14d46d538788ce4b3aad4d9198a6640e
mayra-diaz <mayra.diaz@utec.edu.pe>
2020-12-12 23:12:03 -0500

    Data

--- consultas/consulta3/c3.sql
+++ consultas/consulta3/c3.sql
@@ -1,2 +1,13 @@
 -- Máquinas que hayan utilizado bienes medidos en kilogramos por una cantidad total mayor a 12.
 
+SET SCHEMA 'RT1000';
+
+SELECT T.mcodigo
+FROM Trabajo T, Requiere R, Bien B
+WHERE B.unidades='kg' AND T.fecha BETWEEN '2019-01-01' and '2020-01-01'
+GROUP BY T.mcodigo
+HAVING sum(R.cantidad) > 12;
+
+-- Indices
+-- Hash en B.unidades
+CREATE INDEX 

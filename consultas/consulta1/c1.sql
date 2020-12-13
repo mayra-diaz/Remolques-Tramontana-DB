f8c237cc36d2d3e3577d3315fa31783fac8b1353
lordjuacs <juacsr@gmail.com>
2020-12-13 13:02:00 -0500

    consulta 1 - tiempo de ejecucion y diagrama de ejecucion

--- consultas/consulta1/c1.sql
+++ consultas/consulta1/c1.sql
@@ -1,10 +1,32 @@
 -- Cuántas horas trabajaron por máquina los empleados con los dos sueldos más altos del área de fabricación.
 
-set schema 'rt10000';
-  
-select sum(horas) as HorasTotal from 
-(select tfecha from 
-(select dni from (select dni, sueldo from empleado where area = 'Fabricacion') F2 where sueldo in 
-	(select distinct(sueldo) from (select sueldo from empleado where area = 'Fabricacion') F order by sueldo desc limit 2)) a
-	inner join dirige d on a.dni = d.dni) b inner join trabajo t on b.tfecha = t.fecha;
+SET search_path TO RT1000;
+--CREATE INDEX b_sueldo ON Empleado (sueldo);
+
+
+EXPLAIN ANALYZE
+SELECT sum(horas) AS HorasTotal
+FROM (
+	SELECT tfecha
+	FROM (
+		SELECT dni
+		FROM (
+			SELECT dni
+				,sueldo
+			FROM empleado
+			WHERE area = 'Fabricacion'
+			) F2
+		WHERE sueldo IN (
+				SELECT DISTINCT (sueldo)
+				FROM (
+					SELECT sueldo
+					FROM empleado
+					WHERE area = 'Fabricacion'
+					) F
+				ORDER BY sueldo DESC limit 2
+				)
+		) a
+	INNER JOIN dirige d ON a.dni = d.dni
+	) b
+INNER JOIN trabajo t ON b.tfecha = t.fecha;
 
